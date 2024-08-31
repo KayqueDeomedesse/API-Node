55bb19c03572ab390cbbff1892d4f541334049bb
KayqueDeomedesse <ksd.deomedesse@hotmail.com>
2024-08-31 19:18:16 -0300

    Atividade Express

--- server.js
+++ server.js
@@ -1,127 +1,52 @@
 // Kayque Siqueira Deomedesse
 // 5 AMS (Desenvolvimento de Sistemas Fatec)
-// Atividade Pratica - 1/2 - Criar api com node.js
+// Atividade Pratica - Criar api com Express
 
 
-const { createServer } = require("node:http");
+const express = require('express');
+const { Router } = require('express');
 
-const users = [
-    {
-        id: 1,
-        name: "Mateus Windsor",
-        email: "mateus.windsor@email.com",
-        senha: "mateuswin001",
-    },
+const users = require('./json/users.json')
+const docs = require('./json/docs.json')
 
-    {
-        id: 2,
-        name: "Laura Maria",
-        email: "lauramaria@email.com",
-        senha: "lauran0611",
-    },
+const app = express();
+const route = Router();
 
-     {
-        id: 3,
-        name: "Marina Diomedes",
-        email: "marina.diomedes1@email.com",
-        senha: "marinadc2"
-    },
+app.use(express.json())
 
-    {
-        
-        id: 4,
-        name: "Ana Souza",
-        email: "ana.souza@email.com",
-        senha: "souza@123"
-    },
-
-    {
-        id: 5,
-        name: "Carlos Filho",
-        email: "carlos.filho@email.com",
-        senha: "car@343"
-    }
-];
-
-const docs = [
-    {
-        id: 1,
-        id_user: 1,
-        tipo: "Trabalho",
-        titulo: "Atividade Prática",
-        curso: "ADS",
-        turno: "Manhã"
-    },
-
-    {
-        id: 2,
-        id_user: 2,
-        tipo: "Tarefa",
-        titulo: "Tecnologias Emergentes",
-        curso: "Sistemas de Informação",
-        turno: "Noite"
-    },
-
-    {
-        id: 3,
-        id_user: 3,
-        tipo: "Artigo",
-        titulo: "Projeto Integrador",
-        curso: "Desenvolvimento de Sistemas",
-        turno: "Manhã"
-    },
-
-     {
-    id: 4,
-    id_user: 4,
-    tipo: "Relatório",
-    titulo: "Química Ambiental",
-    curso: "Química",
-    turno: "Tarde"
-    },
-
-    {
-        id: 5,
-        id_user: 5,
-        tipo: "Tese",
-        titulo: "Redes Neurais",
-        curso: "Engenharia Elétrica",
-        turno: "Noite"
-    }
-];
-
-const requestHandler = (req, res) => {
-  const { method, url } = req;
-
+route.get('/', (req, res) => {
   res.setHeader("Content-Type", "application/json");
+  res.end(
+    JSON.stringify({
+      message:
+        "API rodando... entre nas rotas '/users' ou '/docs' ",
+    }))
+})
 
-  if (method === "GET" && url === "/") {
-    res.statusCode = 200;
-    res.end(
-      JSON.stringify({
-        message:
-          "API rodando... entre nas rotas '/users' ou '/docs' ",
-      })
-    );
-  } else if (method === "GET" && url === "/users") {
-    res.statusCode = 200;
-    res.end(JSON.stringify(users));
-  } else if (method === "GET" && url === "/docs") {
-    res.statusCode = 200;
-    res.end(JSON.stringify(docs));
-  } else {
-    res.statusCode = 404;
+route.get('/users', (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.statusCode = 200;
+  res.end(JSON.stringify(users));
+})
+
+route.get('/docs', (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.statusCode = 200;
+  res.end(JSON.stringify(docs));
+})
+
+route.get('*', (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.statusCode = 404;
     res.end(
       JSON.stringify({
         message: `Erro ${res.statusCode}: Essa rota não foi encontrada. Tente novamente utilizando /users ou /docs`,
-      })
-    );
-  }
-};
+      }));
+})
 
-const server = createServer(requestHandler);
 const port = 3300;
 
-server.listen(port, () => {
+app.use(route)
+app.listen(port, () => {
   console.log(`Servidor rodando na porta: http://localhost:${port}/`);
 });
